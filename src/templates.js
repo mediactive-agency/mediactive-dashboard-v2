3039b3f551d428c072ffa091aa3009591cc0ac9b
mediactive-agency <fritsch.krystof@gmail.com>
2026-06-10 01:45:24 +0200

    config: add template sheet IDs

--- src/templates.js
+++ src/templates.js
@@ -2,8 +2,8 @@
 // The /copy URL only works for native Google Sheets shared as
 // "Anyone with the link can view". Leave empty to hide the copy button
 // and offer only the xlsx download.
-export const OUTREACH_TEMPLATE_SHEET_ID = ''
-export const SALES_TEMPLATE_SHEET_ID = ''
+export const OUTREACH_TEMPLATE_SHEET_ID = '1YtTplni2SkwMkNh7uKbVTTRbcsrpiLuUA-UDEKcEZo4'
+export const SALES_TEMPLATE_SHEET_ID = '1tj37ympzYWEWjg3mHcJSUCrqbGZbEfe0fQK2NBdwlIs'
 
 export const TEMPLATES_BASE = import.meta.env.BASE_URL + 'templates/'
 
